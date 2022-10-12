42c37da890ba39e14e841d7a1ef1c468d475f3a0
QiongwenXu <qwxu@outlook.com>
2022-10-12 13:02:33 -0400

    Add irq setup script for arm

---
+++ setup/irq_arm.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Taken from xdp_paper repository
+function root_check_run_with_sudo() {
+    # Trick so, program can be run as normal user, will just use "sudo"
+    #  call as root_check_run_as_sudo "$@"
+    if [ "$EUID" -ne 0 ]; then
+        if [ -x $0 ]; then # Directly executable use sudo
+            echo "Not root, running with sudo"
+            sudo "$0" "$@"
+            exit $?
+        fi
+        err 4 "cannot perform sudo run of $0"
+    fi
+}
+root_check_run_with_sudo "$@"
+
+# Disable Ethernet flow-control, this is network overload test
+echo " --- Disable Ethernet flow-control ---"
+ethtool -A $1 rx off tx off
+
+# For optimal performance align NIC HW queue IRQs
+# and make sure irqbalance don't reorder these
+pkill irqbalance
+
+echo " --- Align IRQs : mlx4_en ---"
+echo "Note: This is supposed to error if it is an Intel NIC"
+for F in /proc/irq/*/mlx4*/../smp_affinity; do
+	dir=$(dirname $F) ;
+	cat $dir/affinity_hint > $F
+done
+grep -H . /proc/irq/*/mlx4*/../smp_affinity_list
