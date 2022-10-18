b9abca699b0c9e62355dcc60a9bbfa95505fbe63
QiongwenXu <qwxu@outlook.com>
2022-10-18 09:34:36 -0400

    Specify force in trex start() function

--- trex/run_trex.py
+++ trex/run_trex.py
@@ -14,7 +14,9 @@ if __name__ == "__main__":
         c.connect() # connect to server
         c.reset(ports = 0)
         c.add_profile(filename="stl/udp_for_benchmarks.py", ports=0, kwargs={"packet_len": 64, "stream_count": 1})
-        c.start(ports = 0, duration = args.time, mult=f"{rate}mpps")
+        # Need to specify 'force', or add the destination mac address in /etc/trex_cfg.yaml
+        # otherwise, not able to send packets (cannot pass the start check)
+        c.start(ports = 0, duration = args.time, mult=f"{rate}mpps", force=True)
         time.sleep(0.5)
         print(f"Start: {rate}")
         while (True):
