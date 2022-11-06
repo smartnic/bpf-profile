ddb987b06096e2308b5fa944686d5c1068b116e4
QiongwenXu <qwxu@outlook.com>
2022-11-06 15:55:34 -0500

    Add a script of preparations for running experiment

---
+++ experiment_prepare.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+dir=experiment
+rm -rf $dir
+mkdir $dir
+cd $dir
+# copy BPF benchmarks
+cp ~/bpf-profile/samples/build .
+# copy profile tools
+cp ~/perf .
+# copy machine config
+cp ~/bpf-profile/profile/config.xl170 .
+# copy profile script
+cp ~/bpf-profile/profile/profile-xdp.py .
