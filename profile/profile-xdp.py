357056d777237eb99d8a728a95d8f8b9d0b4d7e5
QiongwenXu <qwxu@outlook.com>
2023-01-07 15:06:04 -0700

    Add v8 in profile-xdp.py

--- profile/profile-xdp.py
+++ profile/profile-xdp.py
@@ -143,7 +143,7 @@ def get_benchmark_version(prog_name):
         benchmark = BENCHMARK_hhd
     else:
         benchmark = BENCHMARK_xdpex1
-    versions = ["v1", "v2", "v3", "v4", "v5", "v6", "v7"]
+    versions = ["v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8"]
     for v in versions:
         if v in prog_name:
             version = v
