a439e21b21c318ec8283b1f0adc2bfb0887f1fbc
QiongwenXu <qwxu@outlook.com>
2023-04-22 14:42:46 -0600

    Use src mac in RSS rules

--- setup/rss.sh
+++ setup/rss.sh
@@ -9,15 +9,15 @@ if [ -z "$1" ]; then
 fi
 IFACE=$1
 
-SRC_IP_PRE="10.10.1."
-SRC_IP_POST_START=0
+SRC_MAC_PRE="10:10:10:10:10:"
+SRC_MAC_POST_START=0
 NUM_RINGS=$(ethtool -n $IFACE| egrep '[0-9]+ RX rings available' | cut -f 1 -d ' ')
 NUM_CORES=$(grep 'cpu cores' /proc/cpuinfo | uniq | awk '{print $4}')
 num=$(( NUM_RINGS < NUM_CORES ? NUM_RINGS : NUM_CORES ))
 
 for ring in $(seq 0 $(($num - 1))); do
-    ip_post=$((SRC_IP_POST_START + $ring))
-    ethtool -N $IFACE flow-type ip4 src-ip $SRC_IP_PRE$ip_post action $ring
+    ip_post=$((SRC_MAC_POST_START + $ring))
+    ethtool -N $IFACE flow-type ether src $SRC_MAC_PRE$ip_post action $ring
 done
 
 echo "Display rx network flow classification rules"
