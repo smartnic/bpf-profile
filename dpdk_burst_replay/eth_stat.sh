11bcadd1ed0c8faedc043c5ea449b6a51a559b8c
QiongwenXu <qwxu@outlook.com>
2023-05-24 13:28:04 -0400

    Initial commit for measuring performance (dpdk-burst-replay)

---
+++ dpdk_burst_replay/eth_stat.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# By yskoh@mellanox.com
+
+IFG_PLUS_PREAMBLE=20 # IFG 12B + Preamble 8B
+OUTPUT_FILE=eth_stat_output.txt
+
+update_stats () { # $name $index
+	TS_LAST[$2]=${TS[$2]}
+	R_PKT_LAST[$2]=${R_PKT[$2]}
+	R_BYTE_LAST[$2]=${R_BYTE[$2]}
+	T_PKT_LAST[$2]=${T_PKT[$2]}
+	T_BYTE_LAST[$2]=${T_BYTE[$2]}
+
+	ETHTOOL=($(ethtool -S $1 | awk '/tx_packets_phy/{print $2} /rx_packets_phy/{print $2} /tx_bytes_phy/{print $2} /rx_bytes_phy/{print$2}'))
+	if [ -z "$ETHTOOL" ]; then
+		ETHTOOL=($(ethtool -S $1 | awk '/tx_packets/{print $2} /rx_packets/{print $2} /tx_bytes/{print $2} /rx_bytes/{print$2}'))
+	fi
+
+	TS[$2]=$(date +%s%6N) # in usec
+	T_PKT[$2]=${ETHTOOL[0]}
+	R_PKT[$2]=${ETHTOOL[1]}
+	T_BYTE[$2]=${ETHTOOL[2]}
+	R_BYTE[$2]=${ETHTOOL[3]}
+}
+
+if [ -z $1 ] && [ -x /usr/bin/ibdev2netdev ]; then
+	NETIF=$(ibdev2netdev | awk '/mlx/{print $5}')
+else
+	NETIF=$@
+fi
+
+if [ -z "$NETIF" ]; then
+	printf "No interface can't be found\n"
+	exit 1
+fi
+
+# set initial value
+index=0
+for name in $NETIF; do 
+	update_stats $name $index
+	((index++))
+done
+
+index=0
+for name in $NETIF; do 
+	R_PKT_INIT[$index]=${R_PKT[$index]}
+	T_PKT_INIT[$index]=${T_PKT[$index]}
+	R_BYTE_INIT[$index]=${R_BYTE[$index]}
+	T_BYTE_INIT[$index]=${T_BYTE[$index]}
+	((index++))
+done
+
+sleep 0.5
+
+index=0
+for name in $NETIF; do 
+	update_stats $name $index
+
+	TS_DIFF=$((${TS[$index]} - ${TS_LAST[$index]}))
+
+	R_PKT_DELTA=$(( ${R_PKT[$index]} - ${R_PKT_LAST[$index]} ))
+	R_PKT_RATE=$(( $R_PKT_DELTA * 1000000 / $TS_DIFF))
+
+	R_BIT_DELTA=$(( (${R_BYTE[$index]} - ${R_BYTE_LAST[$index]} + $IFG_PLUS_PREAMBLE * $R_PKT_DELTA) * 8 ))
+	R_BIT_RATE=$(( $R_BIT_DELTA * 1000000 / $TS_DIFF))
+
+	T_PKT_DELTA=$(( ${T_PKT[$index]} - ${T_PKT_LAST[$index]} ))
+	T_PKT_RATE=$(( $T_PKT_DELTA * 1000000 / $TS_DIFF))
+
+	T_BIT_DELTA=$(( (${T_BYTE[$index]} - ${T_BYTE_LAST[$index]} + $IFG_PLUS_PREAMBLE * $T_PKT_DELTA) * 8 ))
+	T_BIT_RATE=$(( $T_BIT_DELTA * 1000000 / $TS_DIFF))
+
+	R_PKT_TOTAL=$(( ${R_PKT[$index]} - ${R_PKT_INIT[$index]} ))
+	T_PKT_TOTAL=$(( ${T_PKT[$index]} - ${T_PKT_INIT[$index]} ))
+
+	R_BYTE_TOTAL=$(( ${R_BYTE[$index]} - ${R_BYTE_INIT[$index]} ))
+	T_BYTE_TOTAL=$(( ${T_BYTE[$index]} - ${T_BYTE_INIT[$index]} ))
+
+	# printf "[%'9s Rx]: %'16d pkts  %'16d pps  | %'20d bytes %'16d bps \n" $name $R_PKT_TOTAL $R_PKT_RATE $R_BYTE_TOTAL $R_BIT_RATE
+	# printf "[%'9s Tx]: %'16d pkts  %'16d pps  | %'20d bytes %'16d bps \n" $name $T_PKT_TOTAL $T_PKT_RATE $T_BYTE_TOTAL $T_BIT_RATE
+	str="$R_PKT_RATE $T_PKT_RATE"
+	echo $str > $OUTPUT_FILE
+	((index++))
+done
---
+++ dpdk_burst_replay/measure.py
@@ -0,0 +1,166 @@
+import time
+import subprocess
+import argparse
+import csv
+from os.path import exists
+import os
+import numpy as np
+from statistics import stdev
+
+STATS_FILE = "eth_stat_output.txt"
+
+def run_cmd(cmd, wait=True):
+    # print(cmd)
+    if wait is True:
+        process = subprocess.Popen(cmd, shell=True, close_fds=True)
+        process.wait()
+    else:
+        os.system(cmd)
+
+def get_stats():
+    run_cmd(f"rm -f {STATS_FILE}")
+    get_stats_cmd = "./eth_stat.sh ens114np0"
+    run_cmd(get_stats_cmd) # block until get stats
+    if not exists(STATS_FILE):
+        print(f"ERROR: no such file {STATS_FILE}.")
+        return None
+    stats_list = {}
+    f = open(STATS_FILE, "r")
+    for line in f:
+        str_list = line.split()
+        if len(str_list) < 2:
+            return None
+        stats_list["rx_pps"] = float(str_list[0])
+        stats_list["tx_pps"] = float(str_list[1])
+        stats_list["rx_bps"] = 0
+        stats_list["tx_bps"] = 0
+    f.close()
+    print(stats_list)
+    return stats_list
+
+def wait_until_packet_gen_stable(timeout):
+    t_start = time.time()
+    max_wait_time = float(timeout)
+    time.sleep(2)
+    t_wait_start = time.time()
+    # wait until tx rate is stable
+    tx_before = 0.0
+    while True:
+        wait_time = time.time() - t_wait_start
+        if wait_time >= max_wait_time:
+            return False
+        stats = get_stats()
+        if "tx_pps" not in stats:
+            continue
+        tx_pps = stats["tx_pps"]
+        if tx_pps > 0:
+            variance = abs(tx_pps - tx_before) / tx_pps
+            if variance < 0.01: # tx is stable
+                print(f"variance: {variance}, tx_pps: {tx_pps}")
+                tx_before = tx_pps
+                break
+            tx_before = tx_pps
+        time.sleep(1)
+    print(f"tx stabilize time: {time.time() - t_start}")
+    # wait until rx rate is stable
+    rx_before = 0.0
+    t_start = time.time()
+    while True:
+        wait_time = time.time() - t_wait_start
+        if wait_time >= max_wait_time:
+            return False
+        stats = get_stats()
+        if "rx_pps" not in stats:
+            continue
+        rx_pps = stats["rx_pps"]
+        if rx_pps == rx_before:
+            break
+        if rx_pps > 0:
+            variance = abs(rx_pps - rx_before) / rx_pps
+            if variance < 0.01: # rx is stable
+                print(f"variance: {variance}, rx_pps: {rx_pps}")
+                break
+            rx_before = rx_pps
+        time.sleep(1)
+    print(f"rx stabilize time: {time.time() - t_start}")
+    return True
+
+def measure_performance(duration, output_file):
+    # 1. get statistics every 0.5 second
+    print("Start measurement")
+    rx_pps_list = []
+    tx_pps_list = []
+    rx_bps_list = []
+    tx_bps_list = []
+    min_l_list = []
+    avg_l_list = []
+    max_l_list = []
+    count = 0
+    t_start = time.time()
+    while True:
+        # 2. Check whether to stop measuring
+        if time.time() - t_start > duration:
+            print("Stop measurement")
+            break
+        stats = get_stats()
+        if stats["tx_pps"] > 0:
+            rx_pps_list.append(stats["rx_pps"])
+            tx_pps_list.append(stats["tx_pps"])
+            rx_bps_list.append(stats["rx_bps"])
+            tx_bps_list.append(stats["tx_bps"])
+            count += 1
+            time.sleep(0.5)
+        # mode = 'a'
+        # debug_count += 1
+        # if debug_count == 1:
+        #     mode = 'w'
+        # f = open(f"{output_file}.debug", mode)
+        # writer = csv.writer(f)
+        # lst = [expected_actual_rate, stats["rx_pps"], stats["tx_pps"]]
+        # writer.writerow(lst)
+        # f.close()
+    # 3. Store statistics in the file
+    print("rx_pps_list: ", rx_pps_list)
+    print("tx_pps_list: ", tx_pps_list)
+    rx_pps = np.mean(rx_pps_list)
+    tx_pps = np.mean(tx_pps_list)
+    diff = np.mean(np.abs(np.subtract(rx_pps_list, tx_pps_list)))
+    max_l = 0
+    min_l = 0
+    avg_l = 0
+    print("rx_bps_list: ", rx_bps_list)
+    print("tx_bps_list: ", tx_bps_list)
+    rx_bps = np.mean(rx_bps_list)
+    tx_bps = np.mean(tx_bps_list)
+    diff_bps = np.mean(np.abs(np.subtract(rx_bps_list, tx_bps_list)))
+    print(f"rx = {rx_pps}")
+    print(f"tx = {tx_pps}")
+    print(f"diff = {diff}")
+    print(f"rx_bps = {rx_bps}")
+    print(f"tx_bps = {tx_bps}")
+    f = open(output_file, 'w')
+    writer = csv.writer(f)
+    lst = [count, rx_pps, tx_pps, diff, max_l, min_l, avg_l, rx_bps, tx_bps, diff_bps]
+    writer.writerow(lst)
+    stdev_list = []
+    if count > 0:
+        stdev_list = [stdev(rx_pps_list), stdev(tx_pps_list),
+                      0, 0, 0,
+                      stdev(rx_bps_list), stdev(tx_bps_list)]
+    writer.writerow(stdev_list)
+    writer.writerow(rx_pps_list)
+    writer.writerow(tx_pps_list)
+    writer.writerow(min_l_list)
+    writer.writerow(avg_l_list)
+    writer.writerow(max_l_list)
+    writer.writerow(rx_bps_list)
+    writer.writerow(tx_bps_list)
+    f.close()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Information about Data')
+    parser.add_argument('-t', dest="time", type=int, help='How long(secs) you want to measure performance', default=30)
+    parser.add_argument('-o', dest="output_file", type=str, help='output_file', required=True)
+    args = parser.parse_args()
+    wait_until_packet_gen_stable()
+    measure_performance(args.time, args.output_file)
