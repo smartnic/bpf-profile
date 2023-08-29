c1bddba7faebfd35db6eac8e704d05b76f44ec6e
QiongwenXu <qwxu@outlook.com>
2023-08-29 16:57:19 -0400

    Add pcap generation script for ddos_mitigator

---
+++ dpdk_burst_replay/pcap_scripts/gen_pcap_flow_affinity_ddos_mitigator.py
@@ -0,0 +1,53 @@
+import socket
+import ipaddress
+from scapy.all import *
+from scapy.utils import wrpcap
+import argparse
+import os
+from gen_pcap_utils import *
+
+# Generator function to read and yield packets one by one
+def read_packets(pcap_file):
+    with PcapReader(pcap_file) as pcap_reader:
+        for packet_number, packet in enumerate(pcap_reader, start=1):
+            yield packet_number, packet
+
+def modify_mac_ip_one_pkt(curr_pkt, src_mac, dst_mac, dst_ip):
+    if not curr_pkt.haslayer(IP):
+        return None
+    src_ip = curr_pkt.getlayer(IP).src
+    proto = curr_pkt.getlayer(IP).proto
+    new_pkt = Ether(dst = dst_mac, src = src_mac, type = ETH_P_IP)/ \
+              IP(src = src_ip, dst = dst_ip, proto = proto)/ \
+              curr_pkt[IP].payload
+    return new_pkt
+
+def gen_pcap_flow_affinity_ddos_mitigator(dst_mac, dst_ip, output_path, input_file):
+    print("start [gen_pcap_flow_affinity_ddos_mitigator]")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    # input_pkts = rdpcap(input_file)
+    src_mac = "10:70:fd:d6:a0:1c"
+    new_pkts = []
+    output_file = f"{output_path}/xdp_ddos_mitigator_flow_affinity.pcap"
+    for _, pkt in read_packets(input_file):
+        new_pkt = modify_mac_ip_one_pkt(pkt, src_mac, dst_mac, dst_ip)
+        if new_pkt:
+            new_pkts.append(new_pkt)
+        if len(new_pkts) >= PKTS_WRITE_MAX_NUM:
+            wrpcap(output_file, new_pkts, append=True)
+            # print(f"Written {len(new_pkts)} packets to {output_pcap}")
+            new_pkts = []
+    if new_pkts:
+        wrpcap(output_file, new_pkts, append=True)
+    print(f"[gen_pcap_flow_affinity_ddos_mitigator] output pcap: {output_file}")
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Information about parameters')
+    parser.add_argument('--input', '-i', dest='input_file', help='Input file name', required=True)
+    parser.add_argument("--output", "-o", dest="output_path", help="Output file name", required=True)
+    parser.add_argument("--dst_mac", "-d", dest="dst_mac", help="Destination MAC address to use in the generated PCAP file", default="00:00:00:00:00:02")
+    parser.add_argument("--dst_ip", dest="dst_ip", help="Destination IP address to use in the generated PCAP file ", default="172.16.90.196")
+    args = parser.parse_args()
+    dst_mac = args.dst_mac
+    gen_pcap_flow_affinity_ddos_mitigator(dst_mac, args.dst_ip, args.output_path, args.input_file)
---
+++ dpdk_burst_replay/pcap_scripts/gen_pcap_with_md_ddos_mitigator.py
@@ -0,0 +1,90 @@
+import socket
+import ipaddress
+from scapy.all import *
+from scapy.utils import wrpcap
+import argparse
+import os
+from gen_pcap_utils import *
+
+# struct metadata_elem {
+#   __be16 ethtype;
+#   __be32 ipv4_addr;
+# } __attribute__((packed));
+class MetadataElem():
+  def __init__(self):
+    self.ethtype = 0
+    self.src_ip = 0
+
+  def __str__(self):
+    str = f"Ethtype: {self.ethtype}\n"
+    str += f"Source IP: {ipaddress.IPv4Address(self.src_ip)}\n"
+    return str
+
+  def __bytes__(self):
+    md_bytes = b''
+    md_bytes += self.ethtype.to_bytes(2, 'big')
+    md_bytes += self.src_ip.to_bytes(4, 'big')
+    return md_bytes
+
+
+def get_md_from_pkt(pkt):
+    md_elem = MetadataElem()
+    # ETH_P_IP: 2048 (0x800)
+    md_elem.ethtype = pkt.getlayer(Ether).type
+    md_elem.src_ip = int(ipaddress.ip_address(pkt.getlayer(IP).src))
+    # print(md_elem)
+    return md_elem
+
+# Generator function to read and yield packets one by one
+def read_packets(pcap_file):
+    with PcapReader(pcap_file) as pcap_reader:
+        for packet_number, packet in enumerate(pcap_reader, start=1):
+            yield packet_number, packet
+
+def gen_pcap_with_md_ddos_mitigator(num_cores, dst_mac, output_path, input_file):
+    print(f"start [gen_pcap_with_md_ddos_mitigator] num_cores: {num_cores}")
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+    output_file = f"{output_path}/xdp_ddos_mitigator_shared_nothing_{num_cores}.pcap"
+    # input_pkts = rdpcap(input_file)
+    new_pkts = list()
+    md_initial = MetadataElem()
+    pkt_history = []
+    if num_cores > 1:
+        pkt_history = [md_initial] * (num_cores - 1)
+    for i, curr_pkt in read_packets(input_file):
+        # print(f"\npkt {i}....")
+        # get metadata from curr_pkt
+        md_bytes = b''
+        for x in pkt_history:
+            md_bytes += bytes(x)
+        # src_mac is used for rss
+        src_mac = f"10:10:10:10:10:{format(i % num_cores, '02x')}"
+        # print(src_mac)
+        new_pkt = Ether(dst = dst_mac, src = src_mac, type=ETH_P_IP) / \
+                  md_bytes / \
+                  curr_pkt
+        new_pkts.append(new_pkt)
+        if num_cores > 1:
+            curr_md = get_md_from_pkt(curr_pkt)
+            # update pkt_history
+            pkt_history = pkt_history[1:]
+            pkt_history.append(curr_md)
+        if len(new_pkts) >= PKTS_WRITE_MAX_NUM:
+            wrpcap(output_file, new_pkts, append=True)
+            # print(f"Written {len(new_pkts)} packets to {output_pcap}")
+            new_pkts = []
+    if new_pkts:
+        wrpcap(output_file, new_pkts, append=True)
+    print(f"[gen_pcap_with_md_ddos_mitigator] output pcap: {output_file}")
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Information about parameters')
+    parser.add_argument('--input', '-i', dest='input_file', help='Input file name', required=True)
+    parser.add_argument("--output", "-o", dest="output_path", help="Output file name", required=True)
+    parser.add_argument("--num_cores", "-n", dest="num_cores", help="Number of cores used to process packets", type=int, default=1)
+    parser.add_argument("--dst_mac", "-d", dest="dst_mac", help="Destination MAC address to use in the generated PCAP file", default="00:00:00:00:00:02")
+    args = parser.parse_args()
+    dst_mac = args.dst_mac
+    gen_pcap_with_md_ddos_mitigator(args.num_cores, dst_mac, args.output_path, args.input_file)
